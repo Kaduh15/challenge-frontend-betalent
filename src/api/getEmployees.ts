039b8ea436cea8475fe1222bffe7a19fba27f58c
Kadu <kadu.silva2014@gmail.com>
2025-03-06 13:07:37 -0300

    refactor: adiciona env para url da api
    - adiciona o zod para validadção de dados
    - puxa a url da api pela variaveis de ambiente

--- src/api/getEmployees.ts
+++ src/api/getEmployees.ts
@@ -1,7 +1,10 @@
 import type { Employee } from "../@types/employee";
+import { env } from "../env";
+
+const { VITE_API_URL } = env;
 
 export async function getEmployees() {
-	const response = await fetch("http://localhost:3000/employees");
+	const response = await fetch(`${VITE_API_URL}/employees`);
 	const employees = await response.json();
 	return employees as Employee[];
 }
---
+++ src/env.ts
@@ -0,0 +1,13 @@
+import { z } from "zod";
+
+export const envSchema = z.object({
+  VITE_API_URL: z.string().url().default("http://localhost:3000"),
+});
+
+export type Env = z.infer<typeof envSchema>;
+
+export const env = {
+  ...import.meta.env,
+  ...envSchema.parse(import.meta.env),
+}
+
